77725472f6fba87d82ca033238f39a1a3326fcc0
john joseph <johnpjoseph91@gmail.com>
2018-07-16 15:08:34 +0530

    adding tests

---
+++ src/test/java/util/Util.java
@@ -0,0 +1,50 @@
+package test.java.util;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+
+import java.io.File;
+
+public class Util {
+    public static void writeToFile(String str, String fileName) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), StandardCharsets.UTF_8));
+        writer.write(str);
+        writer.close();
+    }
+
+    public static String readFile(File f){
+        String result = "";
+        InputStream is = null;
+        BufferedReader br = null;
+        try {
+            is = new FileInputStream(f);
+            br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                line = br.readLine();
+            }
+            result = sb.toString();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null)
+                    is.close();
+                if (br != null)
+                    br.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
